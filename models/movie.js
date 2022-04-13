42b8a88aef2713e8992c6a92928f303f35562701
Martin <martin.kaumanns@diebrueder.com>
2022-04-13 16:25:20 +0200

    celebs done

---
+++ models/movie.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    maxlength: 100,
+    required: true,
+    unique: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  plot: {
+    type: String,
+    required: true
+  }
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
